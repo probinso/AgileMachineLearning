6c6ad2e7377ed405a22f604565c531f66fa90af5
probinso <pmoss.robinson@gmail.com>
2016-07-27 15:50:37 -0700

    add gausian logic

--- Models/naivebayes.py
+++ Models/naivebayes.py
@@ -3,9 +3,10 @@ from collections import namedtuple
 from collections import defaultdict
 import pandas as pd
 
+from scipy.stats import norm
+from math import sqrt, exp, pi
 
-
-def wrapper_for_nb_in_sklearn(data, current_state_to_predict):
+def wrapper_for_nb_in_sklearn(data, current_state_to_predict, normal_columns={}):
     """
         Import an already-built implementation, train it on the data,
     and return the class predicted given the current state.
@@ -17,24 +18,35 @@ def wrapper_for_nb_in_sklearn(data, current_state_to_predict):
     store = defaultdict(lambda : defaultdict(dict))
     Label = data.columns[-1]
     maxstore = []
+
+
     for choice in data[Label].unique():
         for col in data.columns[:-1]:
-            for type in data[col].unique():
-                count = len(data[data[col]==type][data[Label]==choice])
-                pop   = len(data[data[Label]==choice])
+            for type in data[col].unique(): 
+                if type in normal_columns:
+                    continue
+                subgraph = data[data[Label]==choice]
+                pop   = len(subgraph)
+                count = len(subgraph[subgraph[col]==type])
+
                 store[choice][col][type] = count/pop
+
+        store[choice].update({col : norm.fit(data[col]) for col in normal_columns})
+
 
     for choice in store:
         prob = len(data[data[Label]==choice])/len(data)
         for index, col in enumerate(data.columns[:-1]):
             state = current_state_to_predict[index]
-            prob *= store[choice][col][state]
+            if col in normal_columns:
+                mu, si = store[choice][col][state]
+                P = lambda x : 1/sqrt(2 * pi * si ** 2) * exp(-1/2 * ((x - mu)/si)**2)
+                state_probability = P(state)
+            else:
+                state_probability = store[choice][col][state]
+
+            prob *= state_probability
 
         maxstore.append((prob, choice))
 
     return max(maxstore)
-
-
-
-
-    return store
