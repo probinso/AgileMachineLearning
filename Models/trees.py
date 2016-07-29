4a6ac0afa8c2e35afb4bce67df16dc1209eaee17
probinso <pmoss.robinson@gmail.com>
2016-07-28 20:21:28 -0700

    first commit

--- Models/trees.py
+++ Models/trees.py
@@ -1,8 +1,15 @@
+from __future__ import print_function, division
+
 import numpy as np
 from sklearn.tree.tree import DecisionTreeClassifier
 
-
 def wrapper_for_decision_tree_in_sklearn(X, y, current_state_to_predict):
-
-    predicted_state = None
+    dtc = DecisionTreeClassifier()
+    dtc.fit(X, y)
+    predicted_state = dtc.predict(current_state_to_predict.reshape(1, -1))
     return predicted_state
+
+def wrapper_for_decision_tree_accuracy(train, test):
+    dtc = DecisionTreeClassifier()
+    dtc.fit(train, test)
+    
--- tests/test_trees.py
+++ tests/test_trees.py
@@ -1,23 +1,27 @@
 from unittest import TestCase
 
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split
 
 from Models.trees import wrapper_for_decision_tree_in_sklearn
-from tests.data import DIGITS_DATASET
+from Models.trees import wrapper_for_decision_tree_accuracy
+from tests.data   import DIGITS_DATASET
 
 class TestDecisionTrees(TestCase):
+    def setUp(self):
+        self.data   = DIGITS_DATASET['data']
+        self.labels = DIGITS_DATASET['target']
+
     def test_digits_solutions_with_pre_built_model(self):
-
-        data = DIGITS_DATASET['data']
-        labels = DIGITS_DATASET['target']
-
         arbitrary_sample_the_default_tree_will_get_right = 3
-
         state_predicted_from_model = wrapper_for_decision_tree_in_sklearn(
-            data, labels, data[arbitrary_sample_the_default_tree_will_get_right])
+            self.data,
+            self.labels,
+            self.data[arbitrary_sample_the_default_tree_will_get_right]
+        )
 
         self.assertEqual(
-            labels[arbitrary_sample_the_default_tree_will_get_right],
+            self.labels[arbitrary_sample_the_default_tree_will_get_right],
             state_predicted_from_model,
             "Solve me first by writing `wrapper_for_decision_tree_in_sklearn`.")
 
@@ -33,6 +37,9 @@ class TestDecisionTrees(TestCase):
             Replace the assertion below with one that checks the model's accuracy
         is greater than or equal to the accuracy_target.
         """
+
+        train, test = train_test_split(self.data, train_size = 0.8)
+
         accuracy_target = 0.8  # percent
         relative_test_set_size = 0.4  # percent
 
