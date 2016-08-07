285d6ac1e7638368ba56b894f5c91510f922b4c4
probinso <pmoss.robinson@gmail.com>
2016-08-07 12:16:39 -0700

    working nerual nets

--- Models/neuralnets.py
+++ Models/neuralnets.py
@@ -1,12 +1,20 @@
 from __future__ import division
+import numpy as np
+
 
 def wrapper_for_backprop_neural_network_code(train_x, train_y, test_x, test_y):
     from sklearn.neural_network import MLPClassifier
-    clf = MLPClassifier(algorithm="l-bfgs", alpha=1e-5, hidden_layer_sizes=(5,2),
-            random_state=6543)
+    clf = MLPClassifier(
+        algorithm="sgd", # stochastic gradient descent
+        hidden_layer_sizes=(100,100),
+        learning_rate='adaptive',
+        random_state=6543,
+        #verbose=True,
+    )
     clf.fit(train_x, train_y)
-    target = clf.predict(test_x)
-    underscore = map(lambda (x,y): x == y, zip(target, test_y))
-    score = sum(underscore) / len(underscore)
 
-    return score
+    target = clf.predict(test_x)
+    score = (target == test_y)
+    _ = np.sum(score) / len(score) # compute hamming distance
+    print(_)
+    return _
